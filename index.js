3219c5548776efb9b273263659fe986595f0692e
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-07-21 11:47:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9irud.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+        const productsCollection = client.db('computerZone').collection('products');
+
+
+        //load services
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        //load a single service 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+
+            const query = { _id: ObjectId(id) }
+            const product = await productsCollection.findOne(query);
+            res.send(product)
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello computer')
+})
+
+app.listen(port, () => {
+    console.log(`computer listening on port ${port}`)
+})
